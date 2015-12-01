38346e6247be66a63882aa4820401333ae4e6299
Martin Urbanec <martin@urbanec.cz>
2015-12-01 19:35:36 +0000

    Add data paring to the script

--- abuseFilter.py
+++ abuseFilter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
+############################ INIT ####################################
 #Importování modulu pro práci s objekty spravovanými interpretem
 import sys
 #Vytvoření proměnné s kódem wikiprojektu
@@ -14,6 +15,8 @@ from wmflabs import db
 #Navázání připojení s databází
 conn = db.connect(wikisite)
 
+######################### ZÍSKÁVÁNÍ DAT ###############################
+
 #Vytvoření databázové transakce
 cur = conn.cursor()
 #Otevření transakce
@@ -22,3 +25,22 @@ with cur:
 	cur.execute('select afl_actions from abuse_filter_log where afl_filter="' + fnum + '"order by afl_timestamp asc')
 	data = cur.fetchall()
 
+
+
+###################### PARSOVÁNÍ DAT ###############################
+
+for_analyze = []
+prevs = []
+prev_article = u""
+for row in data:
+	if row[10] == prev_article:
+		prevs.append(row)
+	else:
+		prevs.append(row)
+		for_analyze.append(prevs)
+		prevs = []
+		prevs.append(row)
+	prev_article = row[10]
+
+import json
+print json.dumps(for_analyze)
