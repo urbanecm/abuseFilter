fa07ba15d11003f41ed9e604879dfa86453bfffd
Martin Urbanec <martin.urbanec@wikimedia.cz>
2015-12-04 19:14:28 +0000

    Add dynamic rendering

--- public/index.php
+++ public/index.php
@@ -23,11 +23,10 @@
         data.addColumn('string', 'Topping');
         data.addColumn('number', 'Slices');
         data.addRows([
-          ['Mushrooms', 3],
-          ['Onions', 1],
-          ['Olives', 1],
-          ['Zucchini', 1],
-          ['Pepperoni', 2]
+	<?
+	$cmd = "~/abuseFilter/analyze.py";
+	echo(shell_exec($cmd));
+	?>
         ]);
 
         // Set chart options
